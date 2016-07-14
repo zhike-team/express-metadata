c93ec592f4a31fd833cbd47e74a5fdd7c352310b
strucoder <zdwloveschina@163.com>
2016-07-14 18:05:41 +0800

    add test

---
+++ test/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const http = require('http');
+const expect = require('chai').expect;
+const metadata = require('..');
+const express = require('express');
+const app = metadata(express());
+const request = require('supertest')(app);
+
+
+
+describe('express metadata', function() {
+  it('should return metadata', function(done) {
+    app.get('/awesome/2', function(req, res) {
+      res.send(app.metadata())
+    });
+
+    let server = http.createServer(app).listen(0);
+    request.get('/awesome/2')
+      .expect(function(res) {
+        expect(res.body).to.have.all.keys('routersSetting', 'projectInfo');
+      })
+      .end(done)
+
+    server.close();
+    });
+
+  it('should return common result', function(done) {
+    app.get('/awesome', function(req, res) {
+      res.send('metadata')
+    });
+
+    let server = http.createServer(app).listen(0);
+
+    request.get('/awesome')
+      .expect(function(res) {
+        expect(res.body).to.match(/metadata/)
+      })
+      .end(done)
+  })
+
+})
